be606d822bd17546ff780af0797c3c02c6273081
example <example@test.test>
2025-06-27 15:34:34 +0300

    Test lefthook 4

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM python:3.13.5-slim-bookworm
 
 WORKDIR /flaskapp
 COPY . .
+# hadolint ignore=DL3008,DL3009,DL3015
 RUN apt-get update && apt-get install python3-venv python3-pip curl -y && python3 -m venv work_env
 ENV PATH="/flaskapp/work_env/bin:$PATH"
-RUN pip3 install -r ./requirements.txt
+RUN pip3 install --no-cache-dir -r ./requirements.txt
 EXPOSE 5000
 CMD ["python3.13", "app.py"]
 
--- app.py
+++ app.py
@@ -3,40 +3,46 @@ from waitress import serve
 import sqlite3
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__, template_folder='./app/templates', static_folder='./app/static')
-app.secret_key = 'very_secret_key'
+app = Flask(__name__, template_folder="./app/templates", static_folder="./app/static")
+app.secret_key = "very_secret_key"
 
 bcrypt = Bcrypt(app)
 
-def connect_db():
-    return sqlite3.connect('main.db')
 
-@app.route('/')
-@app.route('/index')
+def connect_db():
+    return sqlite3.connect("main.db")
+
+
+@app.route("/")
+@app.route("/index")
 def home():
     db = connect_db()
     cur = db.cursor()
-    
-    cur.execute("SELECT news.*, users.id, users.username FROM news, users WHERE news.user_id = users.id")
+
+    cur.execute(
+        "SELECT news.*, users.id, users.username FROM news, users WHERE news.user_id = users.id"
+    )
     news = cur.fetchall()
 
     cur.close()
     db.close()
-    return render_template('pages/index.html', news=news)
+    return render_template("pages/index.html", news=news)
 
-@app.route('/contacts')
+
+@app.route("/contacts")
 def contacts():
-    return render_template('pages/contacts.html')
+    return render_template("pages/contacts.html")
 
-#### NEWS
-@app.route('/profile')
+
+# NEWS
+@app.route("/profile")
 def profile():
     db = connect_db()
     cur = db.cursor()
-    
-    user_id = session['user_id']
 
-    if session['username']:
+    user_id = session["user_id"]
+
+    if session["username"]:
         cur.execute("SELECT * FROM news WHERE user_id = ?", (user_id,))
         news = cur.fetchall()
     else:
@@ -44,132 +50,155 @@ def profile():
 
     cur.close()
     db.close()
-    return render_template('pages/profile.html', news=news)
+    return render_template("pages/profile.html", news=news)
 
-@app.route('/post/<news_id>', methods=['GET'])
+
+@app.route("/post/<news_id>", methods=["GET"])
 def post(news_id):
     db = connect_db()
     cur = db.cursor()
-    
-    cur.execute("SELECT news.*, users.id, users.username FROM news, users WHERE news.user_id = users.id AND news.id = ?", (news_id,))
+
+    cur.execute(
+        "SELECT news.*, users.id, users.username FROM news, users WHERE news.user_id = users.id AND news.id = ?",
+        (news_id,),
+    )
     news = cur.fetchall()
 
     cur.close()
     db.close()
-    return render_template('pages/post.html', news=news)
+    return render_template("pages/post.html", news=news)
 
-##### AUTH
-@app.route('/auth', methods=["GET"])
+
+# AUTH
+@app.route("/auth", methods=["GET"])
 def auth():
-    return render_template('pages/auth.html')
+    return render_template("pages/auth.html")
 
-##### API
-@app.route('/login', methods=["POST"])
+
+# API
+@app.route("/login", methods=["POST"])
 def login():
     db = connect_db()
-    username = request.form['username']
-    password = request.form['password']
-    
+    username = request.form["username"]
+    password = request.form["password"]
+
     cur = db.cursor()
 
     # SQL Injection - 1
     cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
     user = cur.fetchone()
 
-    if user and bcrypt.check_password_hash(password=password.encode(), pw_hash=user[2].encode()):
-        session['user_id'] = user[0]
-        session['username'] = user[1]
+    if user and bcrypt.check_password_hash(
+        password=password.encode(), pw_hash=user[2].encode()
+    ):
+        session["user_id"] = user[0]
+        session["username"] = user[1]
 
         cur.close()
         db.close()
-        return redirect(url_for('home'))
-    
+        return redirect(url_for("home"))
+
     cur.close()
     db.close()
     return "Login failed!", 401
 
-@app.route('/register', methods=["POST"])
+
+@app.route("/register", methods=["POST"])
 def register():
     db = connect_db()
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form["username"]
+    password = request.form["password"]
 
     hashed_password = bcrypt.generate_password_hash(password.encode())
-    
+
     cur = db.cursor()
 
     # SQL Injection - 1
-    cur.execute(f"INSERT INTO users(username, password) VALUES ('{username}', '{hashed_password.decode()}')")
+    cur.execute(
+        f"INSERT INTO users(username, password) VALUES ('{username}', '{hashed_password.decode()}')"
+    )
     db.commit()
 
     cur.close()
     db.close()
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
-@app.route('/update', methods=['POST'])
+
+@app.route("/update", methods=["POST"])
 def update():
     db = connect_db()
     cur = db.cursor()
 
-    user_id = session['user_id']
-    new_password = request.form['new_password']
+    user_id = session["user_id"]
+    new_password = request.form["new_password"]
     new_password_hashed = bcrypt.generate_password_hash(new_password.encode())
 
-    if session['username']:
-        cur.execute(f"UPDATE users SET password = ? WHERE id = ?", (new_password_hashed.decode(), user_id,))
+    if session["username"]:
+        cur.execute(
+            "UPDATE users SET password = ? WHERE id = ?",
+            (
+                new_password_hashed.decode(),
+                user_id,
+            ),
+        )
         cur.close()
         db.commit()
         db.close()
-        return '', 204
-    
+        return "", 204
+
     cur.close()
     db.close()
     return "Access denied", 401
 
-@app.route('/logout', methods=['GET'])
+
+@app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
-@app.route('/delete_news/<news_id>', methods=['POST'])
+
+@app.route("/delete_news/<news_id>", methods=["POST"])
 def delete_news(news_id):
     db = connect_db()
     cur = db.cursor()
 
-    user_id = session['user_id']
-
-    if session['username']:
-        cur.execute(f"DELETE FROM news WHERE id = ?", (news_id,))
+    if session["username"]:
+        cur.execute("DELETE FROM news WHERE id = ?", (news_id,))
         cur.close()
         db.commit()
         db.close()
-        return redirect(url_for('profile'))
-    
+        return redirect(url_for("profile"))
+
     cur.close()
     db.close()
     return "Access denied", 401
 
+
 # XSS Уязвимость - 2
-@app.route('/add_news', methods=['POST'])
+@app.route("/add_news", methods=["POST"])
 def add_news():
     db = connect_db()
     cur = db.cursor()
 
-    user_id = session['user_id']
-    title = request.form['title']
-    imageURL = request.form['title_imageurl']
-    descNews = request.form['description']
+    user_id = session["user_id"]
+    title = request.form["title"]
+    imageURL = request.form["title_imageurl"]
+    descNews = request.form["description"]
 
-    if session['username']:
-        cur.execute(f"INSERT INTO news(user_id, title, title_imageurl, description) VALUES (?, ?, ?, ?)", (user_id, title, imageURL, descNews))
+    if session["username"]:
+        cur.execute(
+            "INSERT INTO news(user_id, title, title_imageurl, description) VALUES (?, ?, ?, ?)",
+            (user_id, title, imageURL, descNews),
+        )
         cur.close()
         db.commit()
         db.close()
-        return redirect(url_for('profile'))
-    
+        return redirect(url_for("profile"))
+
     cur.close()
     db.close()
     return "Access denied", 401
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)
